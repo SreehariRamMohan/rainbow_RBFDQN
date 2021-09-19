a1b92e122a1adcadedd045a6b3c247e475d9f9d8
shangqunyu <>
2021-09-19 19:57:39 -0400

    add visualization for centroid after training

--- experiments/experiment_validation.py
+++ experiments/experiment_validation.py
@@ -8,15 +8,16 @@ import argparse
 import os
 import datetime
 import sys
-
+from sklearn.decomposition import PCA
 sys.path.append("..")
-
+import matplotlib.pyplot as plt
 from common import utils, utils_for_q_learning, buffer_class
 from common.logging_utils import MetaLogger
 
 from rainbow.RBFDQN_rainbow import Net
 from rainbow.RBFDQN_dis import Net as DistributionalNet
-
+from pathlib import Path
+import glob
 import torch
 import numpy
 import gym
@@ -29,7 +30,7 @@ if __name__ == "__main__":
                         type=str)
 
     parser.add_argument("--hyper_parameter_name",
-                        required=True,
+                        required=False,
                         help="0, 10, 20, etc. Corresponds to .hyper file",
                         default="0")  # OpenAI gym environment name
 
@@ -96,6 +97,18 @@ if __name__ == "__main__":
                         help=
                         """Use Distributional RBF-DQN""")
 
+    parser.add_argument("--logged_hyperparams_dir",
+                        type=str,
+                        help="we need that to load the hyper file we are using for the training.",
+                        default="results/HalfCheetah/distributional_sweep/hyperparams/40__seed_0.hyper",
+                        required=False)
+
+    parser.add_argument("--log_centroid_location",
+                        type=utils.boolify,
+                        default=False,
+                        help=
+                        """logging centroid location""")
+
     args, unknown = parser.parse_known_args()
     other_args = {(utils.remove_prefix(key, '--'), val)
                   for (key, val) in zip(unknown[::2], unknown[1::2])}
@@ -106,7 +119,7 @@ if __name__ == "__main__":
     hyperparams_dir = utils.create_log_dir(
         os.path.join(full_experiment_name, "hyperparams"))
 
-    params = utils_for_q_learning.get_hyper_parameters(args.hyper_parameter_name, "rbf")
+    params = utils_for_q_learning.get_hyper_parameters_after_training(args.logged_hyperparams_dir)
 
     params['hyper_parameters_name'] = args.hyper_parameter_name
     params['full_experiment_file_path'] = os.path.join(os.getcwd(), full_experiment_name)
@@ -181,10 +194,33 @@ if __name__ == "__main__":
                                      state_size=len(s0),
                                      action_size=len(env.action_space.low),
                                      device=device)
-
-    Q_object.load_state_dict(torch.load('./results/BipedalWalker/vanilla/logs/target_episode_900'))
+    # here please fill in the main directory for your stored model
+    saved_network_dir = "./results/HalfCheetah/distributional_sweep/logs/"
+    # specify the model that will actually interact with the environment.
+    actor = "target_episode_1700_seed_0"
+    Q_object.load_state_dict(torch.load(saved_network_dir + actor))
     Q_object.eval()
 
+
+    if args.log_centroid_location:
+        Q_objects = {}
+        glob_pattern = saved_network_dir + "target*"
+        for network in glob.glob(glob_pattern):
+            key = network[network.rfind('/')+1:]
+            if not params['distributional']:
+                Q_objects[key] = Net(params,
+                                         env,
+                                         state_size=len(s0),
+                                         action_size=len(env.action_space.low),
+                                         device=device)
+            else:
+                Q_objects[key] = DistributionalNet(params,
+                                                       env,
+                                                       state_size=len(s0),
+                                                       action_size=len(env.action_space.low),
+                                                       device=device)
+                Q_objects[key].load_state_dict(torch.load(network))
+                Q_objects[key].eval()
 
 
     # Logging with Meta Logger
@@ -200,22 +236,36 @@ if __name__ == "__main__":
     all_times_per_steps = []
     all_times_per_updates = []
 
-    num_episodes = 1000
+    num_episodes = 1
     num_success = 0
     for j in range(num_episodes):
         print("episode:", j)
         obs = env.reset()
 
         for i in range(2000):
-            #action = Q_object.e_greedy_policy(obs, j + 1, 'test')
-            action = env.action_space.sample()
+            action = Q_object.e_greedy_policy(obs, j + 1, 'test')
+            #action = env.action_space.sample()
+            if args.log_centroid_location:
+                obs = numpy.array(obs).reshape(1, len(s0))
+                obs = torch.from_numpy(obs).float().to(device)
+                for key in Q_objects:
+
+                    with torch.no_grad():
+                        all_centroids = Q_objects[key].get_centroid_locations(obs).cpu().numpy().squeeze()
+                    pca = PCA(n_components=2)
+                    data = pca.fit_transform(all_centroids)
+                    plt.scatter(data[:,0],data[:,1] , label = key)
+                    plt.legend(loc='upper left')
+                    plt.savefig("centroid_graph/" +str(i)+"_step_"+key)
+                    plt.clf()
+
 
             obs, reward, done, info = env.step(action)
             print("timestep:",i , "getting reward:", reward)
 
             #print("action taken:", action, "finished? ", done)
 
-            env.render()
+            #env.render()
             if done:
                 num_success += 1
                 break
