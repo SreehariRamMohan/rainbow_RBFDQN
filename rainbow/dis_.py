76672b22499e392bac312767259547b3e1662041
Bowen-He <B0wen.He@outlook.com>
2021-12-12 21:48:10 -0500

    add the calculation for CDF

--- rainbow/dis_.py
+++ rainbow/dis_.py
@@ -88,6 +88,9 @@ class Net(nn.Module):
             nn.ReLU(),
             nn.Linear(self.params['layer_size'], self.N),
         )
+        # self.v_min = -400
+        # self.v_max = -100
+        # self.value_range = torch.linspace(self.v_min, self.v_max, self.n_atoms).to(self.device)
 
         if self.params['num_layers_action_side'] == 1:
             self.location_module = nn.Sequential(
@@ -262,42 +265,17 @@ class Net(nn.Module):
         support, indices = torch.sort(support, dim=1)
         prob = prob[X, indices]
         # Support, Prob, Next Support, Next Prob [256, 100]
-        vmin = torch.where(support[:, 0] < next_support[:, 0], support[:, 0], next_support[:, 0]).detach()
-        vmax = torch.where(support[:, -1] > next_support[:, -1], support[:, -1], next_support[:, -1]).detach()
-        vmin = vmin.reshape(-1, 1)
-        vmax = vmax.reshape(-1, 1)
+        multiplier = torch.triu(torch.ones(self.N, self.N))
+        prob = torch.matmul(prob, multiplier)[:, :-1]  # CDF for prob now
+        next_prob = torch.matmul(next_prob, multiplier)[:, :-1]  # CDF for next prob now
+        support = support[:, 1:] - support[:, :-1]
+        next_support = next_support[:, 1:] - next_support[:, :-1]
 
-        y = torch.zeros((batch_size, self.N)).to(self.device)
-        next_v_pos = (next_support - vmin) / ((vmax - vmin) / (self.N - 1))
-        lb = torch.floor(next_v_pos).to(torch.int64).to(self.device)
-        ub = torch.ceil(next_v_pos).to(torch.int64).to(self.device)
-        lb = torch.where(lb > 99, 99, lb)
-        ub = torch.where(ub > 99, 99, ub)
-        lb = torch.where(lb < 0, 0, lb)
-        ub = torch.where(ub < 0, 0, ub)
-        # handling marginal situation for lb==ub
-        lb[(ub > 0) * (lb == ub)] -= 1
-        ub[(lb < (self.N - 1)) * (lb == ub)] += 1
-        offset = torch.linspace(0, ((batch_size - 1) * self.N), batch_size).unsqueeze(1).expand(batch_size, self.N).to(torch.int64).to(self.device)
-        y.view(-1).index_add_(0, (lb + offset).view(-1), (next_prob * (ub.float() - next_v_pos)).view(-1))  # m_l = m_l + p(s_t+n, a*)(u - b)
-        y.view(-1).index_add_(0, (ub + offset).view(-1), (next_prob * (next_v_pos - lb.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
+        area_1 = (prob*support).sum(dim=1)
+        area_2 = (next_prob*next_support).sum(dim=1)
 
-        y_hat = torch.zeros((batch_size, self.N)).to(self.device)
-        v_pos = (support - vmin) / ((vmax - vmin) / (self.N - 1))
-        lb = torch.floor(v_pos).to(torch.int64).to(self.device)
-        ub = torch.ceil(v_pos).to(torch.int64).to(self.device)
-        lb = torch.where(lb > 99, 99, lb)
-        ub = torch.where(ub > 99, 99, ub)
-        lb = torch.where(lb < 0, 0, lb)
-        ub = torch.where(ub < 0, 0, ub)
-        # handling marginal situation for lb==ub
-        lb[(ub > 0) * (lb == ub)] -= 1
-        ub[(lb < (self.N - 1)) * (lb == ub)] += 1
-        offset = torch.linspace(0, ((batch_size - 1) * self.N), batch_size).unsqueeze(1).expand(batch_size, self.N).to(torch.int64).to(self.device)
-        y_hat.view(-1).index_add_(0, (lb + offset).view(-1), (prob * (ub.float() - v_pos)).view(-1))  # m_l = m_l + p(s_t+n, a*)(u - b)
-        y_hat.view(-1).index_add_(0, (ub + offset).view(-1), (prob * (v_pos - lb.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
-        # breakpoint()
-        loss = torch.sum((-y * torch.log(y_hat + 0.0001)), 1)  # (m , N_ATOM)
+        loss = area_2 - area_1
+        # loss = torch.sum((-y * torch.log(y_hat + 0.0001)), 1)  # (m , N_ATOM)
         loss = torch.mean(loss)
         # print(loss)
         self.zero_grad()
